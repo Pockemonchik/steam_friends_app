be9c9c50b0e094ba2b0db5259f965e1d55156586
Pockemonchik <19pokemonchik99@gmail.com>
2023-11-22 21:05:10 +0300

    chekc friends by command

--- main.py
+++ main.py
@@ -1,43 +1,109 @@
 import logging
 import sys
 import asyncio
+from re import Match
 from aiogram import Bot, Dispatcher,  types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram.utils import markdown
 from aiogram.enums import ParseMode
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from config import settings
+from aiogram import F
+import steam_service
+
 
 
 dp = Dispatcher()
 
+
+HELP_COMMAND = """
+<b>/help</b> - <em>список команд</em>
+<b>/start</b> - <em>старт бота</em>
+<b>/description</b> - <em>описание бота</em>
+<b>/friends</b> - <em>друзья в  сети</em>"""
+
+FRIEND_LIST = """
+<b>Друг 1</b> - <em>в сети</em>
+<b>Друг 2</b> - <em>Дота 2</em>
+"""
+ID_REGEX = r'\d{17}$'
 
 @dp.message(CommandStart())
 async def start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
     """
-    await message.answer(f"Hello, {(message.from_user.full_name)}!")
+    kb = [
+        [types.KeyboardButton(text="/help")],
+        [types.KeyboardButton(text="/description")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb,
+                                         resize_keyboard=True,
+                                         input_field_placeholder="Что будем делать?")
+    await message.answer(f"Hello, {(message.from_user.full_name)}", reply_markup=keyboard)
 
 
-@dp.message(Command("help"))
-async def help_handler(message: Message) -> None:
+@dp.message(Command('help'))
+async def help_handler(message: types.Message):
+    await message.answer(text=HELP_COMMAND,
+                         parse_mode="HTML")
+    # await message.delete()
+
+
+@dp.message(Command('friends'))
+async def friends_handler(message: types.Message):
+    steam_data = steam_service.get_steam_user_friends_info("76561198381522154")
+    import json
+    gaming_friends = (list(filter(lambda x: "gameextrainfo" in x,steam_data["friends"])))
+    # print(type(steam_data["fiends"]))
+    print(json.dumps(gaming_friends))
+    friend_list = []
+    for friend in gaming_friends:
+        friend_list.append("<b>{}</b> - <em>{}</em>".format(friend['personaname'],friend['gameextrainfo']))
+    print("friend_list",friend_list)
+    await message.answer(text="\n".join(friend_list),
+                         parse_mode="HTML")
+    # await message.delete()
+
+
+
+
+@dp.message(Command("description"))
+async def description_handler(message: Message) -> None:
     text = markdown.text(
-        markdown.markdown_decoration.quote("Бот предназначен для отслеживания\nактивности друзей в Steam\n"),
+        markdown.markdown_decoration.quote(
+            "Бот предназначен для отслеживания\nактивности друзей в Steam\n"),
         markdown.text(
-            markdown.bold("Список команд\n"),
-            markdown.underline("Добавить друга для отслеживания"),
+            markdown.bold(
+                "Для начала необходимо зарегистрировать\nсвой steam id \n"),
+            markdown.underline("Зарегистрировать"),
             sep="\n",
         ),
         sep="\n",
     )
 
     await message.answer(text=text,
-                        #  parse_mode=None,
+                         #  parse_mode=None,
                          parse_mode=ParseMode.MARKDOWN_V2,
                          )
 
+
+@dp.message(F.text.regexp(ID_REGEX).as_("digits"))
+async def id_handler(message: types.Message, digits: Match[str]):
+    """Привязка steam id к ползователю"""
+    print("found id")
+    await message.answer(text=message.text,
+                         parse_mode="HTML")
+    # await message.delete()
+
+@dp.message()
+async def not_hadle__handler(message: types.Message):
+    """если не отловлено"""
+    await message.answer(text=message.text,
+                         parse_mode="HTML")
+    # await message.delete()
 
 async def main() -> None:
     bot = Bot(
@@ -45,7 +111,7 @@ async def main() -> None:
         # token=settings.bot_token,
         parse_mode=ParseMode.MARKDOWN_V2,
     )
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, skip_updates=True)
 
 
 if __name__ == "__main__":
--- steam_service.py
+++ steam_service.py
@@ -1,12 +1,11 @@
 from steam import Steam
-from decouple import config
-
+import asyncio
 KEY = "53DCDE6701B169E877EADFDB023D78EA"
 steam = Steam(KEY)
 # steam id 76561198381522154
-def get_steam_user_info(steam_id:str)-> dict:
+def get_steam_user_info(steam_id:str):
     return steam.users.get_user_details(steam_id)
 
-def get_steam_user_friends_info(steam_id:str)-> dict:
-    return steam.users.get_user_friends_list(steam_id)
+def get_steam_user_friends_info(steam_id:str):
+    return (steam.users.get_user_friends_list(steam_id))
 print(steam.users.search_user("76561198381522154"))
