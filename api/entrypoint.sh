f2ded9eeca187a93ab92897a2ae0aabab34c537e
pockemonchik <19pokemonchik99@gmail.com>
2024-03-01 22:46:32 +0300

    add alembic

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#alembic init -t async migrations
+# alembic revision -m "initial"
 echo "start fastapi app";
 echo "";
+exec "alembic upgrade head"
 exec "$@"
---
+++ api/src/models/subscribe_model.py
@@ -0,0 +1,14 @@
+from .base_model import BaseModel
+from sqlalchemy.orm import Mapped 
+from schemas.user_schema import UserSchema
+
+
+class Subscribe(BaseModel):
+    username: Mapped[str]
+    steam_token: Mapped[str]
+    def to_read_model(self) -> UserSchema:
+        return UserSchema(
+            id=self.id,
+            username=self.username,
+            steam_token=self.steam_token,
+        )
